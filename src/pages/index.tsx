c91b667e7169c32081485bbed4225a7df9c0dac9
cassia.herzog <cassiahherzog@gmail.com>
2022-12-05 14:56:00 -0300

    frist commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold underline  text-red-200 ">Born to be wild</h1>
+    <div></div>
+    </div>
+  )
+}
